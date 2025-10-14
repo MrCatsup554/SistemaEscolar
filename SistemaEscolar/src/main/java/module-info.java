7a876a7aa663bc7b5f2c769cd36c4651ffada81d
Moises <al072954@uacam.mx>
2025-10-14 12:20:22 -0600

    fead: Creada clase controladora para la base de datos

--- SistemaEscolar/src/main/java/module-info.java
+++ SistemaEscolar/src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module SistemaEscolar {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.jcraft.jsch;
+    requires java.sql;
 
     opens org.example to javafx.fxml, javafx.graphics;
 }
---
+++ SistemaEscolar/src/main/java/org/example/ConexionBD.java
@@ -0,0 +1,54 @@
+package org.example;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+
+import java.sql.*;
+
+public class ConexionBD {
+    String hostname = "fi.jcaguilar.dev";
+    String sshUser = "patito";
+    String sshPass = "cuack";
+
+    String dbUser = "becario";
+    String dbPass = "FdI-its-5a";
+    String conString;
+
+    public ConexionBD() throws JSchException{
+        JSch jsch = new JSch();
+
+        //ssh patito@fi.jcaguilar.dev
+        Session sesion = jsch.getSession(sshUser, hostname);
+
+        //Introducir contraseña
+        sesion.setPassword(sshPass);
+
+        //Deshabilita los mensajes de error
+        sesion.setConfig("StrictHostKeyChecking", "no");
+
+        //Obtenemos un puerto redireccionando
+        sesion.connect();
+        int port = sesion.setPortForwardingL(0, "localhost", 3306);
+
+        conString = "jdbc:mariadb://localhost:" + port + "/its5a";
+
+        System.out.println("Conexion exitosa a la base de datos");
+    }
+
+    public void selectPersonas(int id_persona){
+        try(Connection con = DriverManager.getConnection(conString, dbUser, dbPass)) {
+            Statement sentencia = con.createStatement();
+            String sql =
+                    "select nombre, apellido from personas_escuela where id_persona = " + id_persona + ";";
+            ResultSet resultado = sentencia.executeQuery(sql);
+
+            while(resultado.next()){
+                String nombre = resultado.getString(1);
+                String apellido = resultado.getString(2);
+                System.out.println("Persona: " + nombre + " " + apellido);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
